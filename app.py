755aa86e51c3078b36efe05deeae0a97422d6181
Zephyrus02 <aneeshraskar@gmail.com>
2024-10-03 01:58:44 +0530

    Added course recommendations

--- app.py
+++ app.py
@@ -16,5 +16,18 @@ def send_file_path():
 
     return JobRole
 
+@app.route('/get-courses', methods=['POST'])
+def get_courses():
+    data = request.get_json()
+    file_path = data.get('file_path')
+    
+    if not file_path:
+        return jsonify({'error': 'File path not provided'}), 400
+
+    response_string = rp.pdfToStr(file_path)
+    courses = rp.get_recommended_courses(response_string)
+
+    return courses
+
 if __name__ == '__main__':
     app.run(debug=True, port=5001)

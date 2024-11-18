6fe47a0498ba8adc0b6c1d3ae64b7a8c6c44fbfc
Zephyrus02 <aneeshraskar@gmail.com>
2024-11-18 17:43:00 +0530

    Removed pdfToString

--- app.py
+++ app.py
@@ -3,31 +3,30 @@ import utils.resume_parser as rp # available functions: pdfToStr, ResumeParser
 
 app = Flask(__name__)
 
-@app.route('/send-file-path', methods=['POST'])
-def send_file_path():
+@app.route('/get-role', methods=['POST'])
+def predictRes():
     data = request.get_json()
-    file_path = data.get('file_path')
+    text = data.get('text')
     
-    if not file_path:
-        return jsonify({'error': 'File path not provided'}), 400
+    if not text:
+        return jsonify({'error': 'File not provided'}), 400
 
-    response_string = rp.pdfToStr(file_path)
-    JobRole = rp.ResumeParser(response_string)
+    JobRole = rp.ResumeParser(text)
+    print("Job Role: ", JobRole)
 
     return JobRole
 
-@app.route('/get-courses', methods=['POST'])
-def get_courses():
-    data = request.get_json()
-    file_path = data.get('file_path')
+# @app.route('/get-courses', methods=['POST'])
+# def get_courses():
+#     data = request.get_json()
+#     text = data.get('text')
     
-    if not file_path:
-        return jsonify({'error': 'File path not provided'}), 400
+#     if not text:
+#         return jsonify({'error': 'File not provided'}), 400
 
-    response_string = rp.pdfToStr(file_path)
-    courses = rp.get_recommended_courses(response_string)
+#     courses = rp.get_recommended_courses(text)
 
-    return courses
+#     return courses
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
--- utils/resume_parser.py
+++ utils/resume_parser.py
@@ -8,25 +8,6 @@ API_KEY = 'your_api_key'
 os.environ["API_KEY"] = API_KEY
 genai.configure(api_key=os.environ["API_KEY"])
 
-def pdfToStr(pdf_path):
-    try:
-        doc = fitz.open(pdf_path)
-        text_content = ""
-
-        for page_num in range(doc.page_count):
-            page = doc.load_page(page_num)
-            text_content += page.get_text()
-
-        doc.close()
-
-        formatted_text = ' '.join(text_content.split())
-
-        return formatted_text
-
-    except Exception as e:
-        error_msg = f"An error occurred while processing the PDF: {e}"
-        return error_msg
-
 def ResumeParser(resume_text):
 
     max_length=512
